d671d13b20ae0be0eb8b1589cf840b1421413a17
mrtaln <murat.alan@ymail.com>
2020-12-08 23:01:31 +0300

    Navbar added.

---
+++ src/components/Navbar.vue
@@ -0,0 +1,19 @@
+<template>
+    <nav>
+        <v-toolbar :color="'grey darken-4'">
+            <v-toolbar-items>
+                <div>
+                    <v-btn outlined color="primary" dark>
+                        Rounded Button
+                    </v-btn>
+                </div>
+            </v-toolbar-items>
+        </v-toolbar>
+    </nav>
+</template>
+
+<script>
+export default {
+    
+}
+</script>
